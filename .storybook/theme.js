78066e67975984a9302387db421ff75d2ce95644
Rob Squires <rsquires@sans.org>
2020-12-12 17:23:00 +0000

    chore: storybook clean p

---
+++ .storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+import { themes } from '@storybook/theming';
+
+const theme = create({
+  brandTitle: 'BTFDS',
+  brandUrl: 'https://www.bristoltechfair.org/',
+  base: 'light'
+});
+
+export default theme;
