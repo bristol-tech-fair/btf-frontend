12a5ee8aaa69d01c707a73ff52d1a94e699f5b6f
Rob Squires <rsquires@sans.org>
2020-12-13 22:19:07 +0000

    chore: move docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../docs/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-controls',
     '@storybook/addon-links',
