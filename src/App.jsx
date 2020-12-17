3c5adb6e08c6170fb59c3fa9b2554627c234b515
Rob Squires <rsquires@sans.org>
2020-12-17 19:36:44 +0000

    feat: router init

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Theme from './theme';
+import Router from './router';
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Theme />
-      <h1>h1</h1>
-      <h2>h2</h2>
-      <h3>h3</h3>
-      <h4>h4</h4>
-      <p>p</p>
-      <a href="#/">a</a>
-    </>
+      <Router />
+    </BrowserRouter>
   );
 };
 
---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+const Router = () => {
+  <Switch>
+    <Route />
+  </Switch>;
+};
+
+export default Router;
