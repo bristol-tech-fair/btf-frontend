97239ce80130dd078bf131b507ecbccf3755ba73
Shane Mitchell <shaneSC@gmail.com>
2021-01-15 19:02:21 +0000

    test: add tests for PrimaryButton component

--- src/components/PrimaryButton/PrimaryButton.test.jsx
+++ src/components/PrimaryButton/PrimaryButton.test.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 import PrimaryButton from './PrimaryButton';
 
 describe('PrimaryButton', () => {
   test('renders PrimaryButton component', () => {
-    render(<PrimaryButton value="Hello" />);
+    render(<PrimaryButton content="Primary button" />);
 
-    expect(screen.getByText('Hello'));
+    expect(screen.getByRole('button')).toHaveTextContent('Primary button');
+  });
+
+  test('renders button of type submit', () => {
+    render(<PrimaryButton content="Primary button" type="submit" />);
+
+    expect(screen.getByRole('button')).toHaveAttribute('type', 'submit');
+  });
+
+  test('calls onClick prop when clicked', () => {
+    const handleClick = jest.fn();
+
+    render(<PrimaryButton content="Primary button" onClick={handleClick} />);
+
+    fireEvent.click(screen.getByRole('button'));
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
