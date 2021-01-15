917ffae3ec0b059d57ecf73a6b979f9c4ea85dff
Shane Mitchell <shaneSC@gmail.com>
2021-01-15 18:18:20 +0000

    feat: add active and focus styles to button

--- src/components/PrimaryButton/PrimaryButton.styles.js
+++ src/components/PrimaryButton/PrimaryButton.styles.js
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
 
 export const PrimaryButtonWrapper = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: var(--small-space) var(--large-space);
-  border-radius: 5px;
-  background: var(--lightblue);
-  color: var(--lightgrey);
-  font-weight: 700;
+  &,
+  &:active,
+  &:focus {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: var(--small-space) var(--large-space);
+    border-radius: 5px;
+    background: var(--lightblue);
+    color: var(--lightgrey);
+    font-weight: 700;
+  }
 
   &:hover {
     background: var(--logopink);
