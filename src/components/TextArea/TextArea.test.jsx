77f2217f7093c0160b684a34abde73659706b243
Cath Howard <cath.m.howard@gmail.com>
2021-01-15 18:44:43 +0000

    Added TextArea component tests

--- src/components/TextArea/TextArea.test.jsx
+++ src/components/TextArea/TextArea.test.jsx
@@ -4,9 +4,19 @@ import { render, screen } from '@testing-library/react';
 import TextArea from './TextArea';
 
 describe('TextArea', () => {
-  test('renders TextArea component', () => {
-    render(<TextArea value="Hello" />);
+  test('renders the correct label text', () => {
+    render(<TextArea labelText="Label text" placeholder="Input text" />);
 
-    expect(screen.getByText('Hello'));
+    expect(screen.getByText('Label text'));
+  });
+  test('renders a textarea input', () => {
+    render(<TextArea labelText="Label text" placeholder="Input text" />);
+
+    expect(TextArea).toBeDefined();
+  });
+  test('renders the correct placeholder text', () => {
+    render(<TextArea labelText="Label text" placeholder="Input text" />);
+
+    expect(screen.getByPlaceholderText('Input text'));
   });
 });
