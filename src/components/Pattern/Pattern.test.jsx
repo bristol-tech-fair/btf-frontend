c96112d02a6f0a2bc17562b09fee19b6e3221d6d
Michael Burridge <mburridge@zyriab.co.uk>
2021-01-17 13:26:10 +0000

    test: adds tests for Pattern component

--- src/components/Pattern/Pattern.test.jsx
+++ src/components/Pattern/Pattern.test.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 import Pattern from './Pattern';
 
 describe('Pattern', () => {
   test('renders Pattern component', () => {
-    render(<Pattern value="Hello" />);
+    const { container } = render(<Pattern color="logopink" size="L" />);
 
-    expect(screen.getByText('Hello'));
+    const svg = container.querySelector('svg');
+    const paths = container.querySelectorAll('path');
+
+    // expect(screen.getByText('Hello'));
+    screen.debug();
+    expect(svg).toHaveAttribute('width', '1377'); // S: 451 || M: 938 || L: 1377
+    expect(paths[0].classList.contains('logopink')).toBe(true);
   });
 });
