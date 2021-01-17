c6b5d9e49af19931509f854aacdede9bd3e2d36d
Shane Mitchell <shaneSC@gmail.com>
2021-01-17 18:47:01 +0000

    feat: use rems instead of px, add active and focus styling

--- src/components/PrimaryButton/PrimaryButton.styles.js
+++ src/components/PrimaryButton/PrimaryButton.styles.js
@@ -1,21 +1,21 @@
 import styled from 'styled-components';
 
 export const PrimaryButtonWrapper = styled.button`
-  &,
-  &:active,
-  &:focus {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: var(--small-space) var(--large-space);
-    border-radius: 5px;
-    background: var(--lightblue);
-    color: var(--lightgrey);
-    font-weight: 700;
-  }
+  padding: var(--small-space) var(--large-space);
+  border-radius: 0.5rem;
+  background: var(--lightblue);
+  color: var(--lightgrey);
+  font-weight: 700;
 
   &:hover {
     background: var(--logopink);
     color: var(--lightgrey);
+  }
+
+  &:active,
+  &:focus {
+    background: var(--logopink);
+    color: var(--lightgrey);
+    box-shadow: 0 0 0 0.4rem var(--darkpink);
   }
 `;
