5849434c618d23d2c70dd78bc7f20852988e1dbd
Tomasz <admtomas@gmail.com>
2021-01-18 21:01:26 +0000

    HeartFull fill error

--- src/components/Icons/Icons.stories.jsx
+++ src/components/Icons/Icons.stories.jsx
@@ -19,3 +19,18 @@ export const HeartFull = () => (
   />
 );
 export const Youtube = () => <Icons icon={ICONS.Youtube} />;
+export const Message = () => <Icons icon={ICONS.Message} />;
+export const Clock = () => <Icons icon={ICONS.Clock} />;
+export const ArrowCircleLeft = () => <Icons icon={ICONS.ArrowCircleLeft} />;
+export const ArrowCircleRight = () => <Icons icon={ICONS.ArrowCircleRight} />;
+export const Share = () => <Icons icon={ICONS.Share} />;
+export const Twitter = () => <Icons icon={ICONS.Twitter} />;
+export const LinkedIn = () => <Icons icon={ICONS.LinkedIn} />;
+export const Facebook = () => <Icons icon={ICONS.Facebook} />;
+export const Bookmark = () => <Icons icon={ICONS.Bookmark} />;
+export const ArrowRight = () => <Icons icon={ICONS.ArrowRight} />;
+export const ArrowLeft = () => <Icons icon={ICONS.ArrowLeft} />;
+export const Cross = () => <Icons icon={ICONS.Cross} />;
+export const Image = () => <Icons icon={ICONS.Image} />;
+export const Link = () => <Icons icon={ICONS.Link} />;
+export const Menu = () => <Icons icon={ICONS.Menu} />;
