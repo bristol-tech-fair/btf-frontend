bbe630a1820d59babbe7e9343722d73c58bc0f50
Tomasz <admtomas@gmail.com>
2021-01-18 20:35:14 +0000

    Testing passing props to component

--- src/components/Icons/Icons.stories.jsx
+++ src/components/Icons/Icons.stories.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import Icons from './Icons';
+import { ICONS } from './paths';
 
 export default {
   title: 'Components/Icons',
   component: Icons
 };
 
-export const HelloWorld = () => <Icons value="Hello world!" />;
+export const File = () => <Icons icon={ICONS.File} />;
+export const Heart = () => (
+  <Icons icon={ICONS.Heart} stroke="var(--lightpink)" />
+);
+export const HeartFull = () => (
+  <Icons
+    icon={ICONS.HeartFull}
+    stroke="var(--lightpink)"
+    fill="var(--lightpink)"
+  />
+);
+export const Youtube = () => <Icons icon={ICONS.Youtube} />;
