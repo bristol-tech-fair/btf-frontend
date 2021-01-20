f1e55f1e411d05422f667016aeb9ec1fdaa43df1
r1903 <55586316+r1903@users.noreply.github.com>
2021-01-20 21:51:39 +0000

    style:added height to the logo image

--- src/components/Logo/Logo.styles.js
+++ src/components/Logo/Logo.styles.js
@@ -6,5 +6,6 @@ export const LogoWrapper = styled.img.attrs((props) => ({
   src: props.color === 'red' ? `${LogoRed}` : `${LogoBlue}`,
   alt: 'Bristol Tech Fair Logo'
 }))`
-  width: 20rem;
+  width: 18.8rem;
+  height: 8.2rem;
 `;
