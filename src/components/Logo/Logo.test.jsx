ec7c7f5aed4a640fbc94bc7218301a68ec7b8f61
r1903 <55586316+r1903@users.noreply.github.com>
2021-01-22 00:47:52 +0000

    test:changed tests to support new changes

--- src/components/Logo/Logo.test.jsx
+++ src/components/Logo/Logo.test.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { toHaveStyle } from '@testing-library/jest-dom';
 
 import Logo from './Logo';
 
 describe('Logo', () => {
-  test('renders Logo component', () => {
-    const { getByAltText } = render(<Logo color="red" />);
+  test('renders Logo component with default width', () => {
+    const { container } = render(<Logo />);
 
-    expect(getByAltText('Bristol Tech Fair Logo')).toBeTruthy();
+    expect(container.firstChild).toHaveStyle(`width:18.8rem`);
+  });
+
+  test('renders Logo component with given width', () => {
+    const { container } = render(<Logo width={10} />);
+
+    expect(container.firstChild).toHaveStyle(`width:10rem`);
   });
 });
