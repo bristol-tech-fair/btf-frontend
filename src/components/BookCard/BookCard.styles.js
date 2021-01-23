861ccc866d2ac6b8bd09bf3bd6befa962a234afb
Shane Mitchell <shaneSC@gmail.com>
2021-01-23 19:04:32 +0000

    feat: Add BookCard styling

--- src/components/BookCard/BookCard.styles.js
+++ src/components/BookCard/BookCard.styles.js
@@ -1,6 +1,28 @@
 import styled from 'styled-components';
 
 export const BookCardWrapper = styled.div`
-  padding: var(--small-space);
-  border: 0.1rem solid var(--logopink);
+  width: 24rem;
+  height: 30.9rem;
+  background: #ffffff;
+  border: 0.1rem solid var(--lightblue);
+  border-radius: 1rem;
+  overflow: hidden;
+  display: inline-block;
+  text-align: center;
+
+  & > img {
+    height: 14rem;
+    width: 100%;
+    object-fit: cover;
+    margin-bottom: var(--small-space);
+  }
+
+  & > h3 {
+    margin: 0 var(--medium-space) var(--small-space);
+  }
+
+  & > span {
+    font-size: 1.6rem;
+    font-weight: 700;
+  }
 `;
