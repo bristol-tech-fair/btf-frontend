1cc407b33fb58368fbd14431ef75327b68179433
Tumeka Burston <burston1993@icloud.com>
2021-01-26 15:16:27 +0000

    style: input styling inline with figma design

--- src/components/TextInput/TextInput.styles.js
+++ src/components/TextInput/TextInput.styles.js
@@ -4,12 +4,20 @@ export const TextInputWrapper = styled.div`
   input {
     padding: var(--small-space);
     border: 0.1rem solid var(--lightblue);
-    background: #f2f2f2;
+    background: var(--lightgrey);
     border-radius: 0.5rem;
+    width: 30rem;
+    height: 6rem;
   }
 
   label {
     display: flex;
     align-items: center;
+    color: var(--darkblue);
+    font-weight: bold;
+  }
+
+  placeholder {
+    color: var(--darkgrey);
   }
 `;
