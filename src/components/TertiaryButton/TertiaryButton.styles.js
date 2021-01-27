a8d6d3a1da5a2933fb51a8fc5570cc99b1316a6c
Tarik <tarikmerrylees@gmail.com>
2021-01-27 19:26:02 +0000

    additional styling corrections

--- src/components/TertiaryButton/TertiaryButton.styles.js
+++ src/components/TertiaryButton/TertiaryButton.styles.js
@@ -8,4 +8,15 @@ export const TertiaryButtonWrapper = styled.button`
   border: 0.1rem solid;
   border-color: var(--lightblue);
   border-radius: 10rem;
+
+  &:hover {
+    color: var(--logopink);
+    border-color: var(--logopink);
+  }
+
+  &:active,
+  &:focus {
+    color: var(--logopink);
+    box-shadow: 0 0 0 0.4rem var(--logopink);
+  }
 `;
