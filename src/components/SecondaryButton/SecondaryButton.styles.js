452feb718fe343ec7026a2843ff64b64f1872862
Sophie Stammers <sophie.stammers@gmail.com>
2021-01-27 19:40:26 +0000

    style: active & focus styling in line with primary btn

--- src/components/SecondaryButton/SecondaryButton.styles.js
+++ src/components/SecondaryButton/SecondaryButton.styles.js
@@ -1,18 +1,20 @@
 import styled from 'styled-components';
 
 export const SecondaryButtonWrapper = styled.button`
-&,
-&:active,
-&:focus {    
-  display: flex;
-  justify-content: center;
-  align-items: center;
   background: var(--lightblue);
   color: var(--lightgrey);
   padding: var(--smallest-space) var(--medium-space);
-  border-radius: 6.25rem;
+  border-radius: 10rem;
+
   &:hover {
     background: var(--logopink);
     color: var(--lightgrey);
+  }
+
+  &:active,
+  &:focus {
+    background: var(--logopink);
+    color: var(--lightgrey);
+    box-shadow: 0 0 0 0.4rem var(--darkpink);
   }
 `;
