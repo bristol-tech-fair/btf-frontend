a1162c90628d60c36a6bd018bec030a15ae63cd8
Tarik <tarikmerrylees@gmail.com>
2021-01-27 19:21:07 +0000

    tests written

--- src/components/TertiaryButton/TertiaryButton.test.jsx
+++ src/components/TertiaryButton/TertiaryButton.test.jsx
@@ -5,8 +5,24 @@ import TertiaryButton from './TertiaryButton';
 
 describe('TertiaryButton', () => {
   test('renders TertiaryButton component', () => {
-    render(<TertiaryButton value="Tertiary Button" />);
+    render(<TertiaryButton content="Tertiary button" />);
 
-    expect(screen.getByText('Tertiary Button'));
+    expect(screen.getByRole('button')).toHaveTextContent('Tertiary button');
+  });
+
+  test('renders button of type submit', () => {
+    render(<TertiaryButton content="Tertiary button" type="submit" />);
+
+    expect(screen.getByRole('button')).toHaveAttribute('type', 'submit');
+  });
+
+  test('calls onClick prop when clicked', () => {
+    const handleClick = jest.fn();
+
+    render(<TertiaryButton content="Tertiary button" onClick={handleClick} />);
+
+    fireEvent.click(screen.getByRole('button'));
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
