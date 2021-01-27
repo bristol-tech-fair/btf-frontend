830d50ad5d742c30d9b52322cca393a700f70a56
Tumeka Burston <burston1993@icloud.com>
2021-01-27 19:30:42 +0000

    styles: removed display:flex from label and added to global input wrapper

--- src/components/TextInput/TextInput.styles.js
+++ src/components/TextInput/TextInput.styles.js
@@ -1,21 +1,14 @@
 import styled from 'styled-components';
 
 export const TextInputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
   input {
-    position: absolute;
     padding: var(--small-space);
     border: 0.1rem solid var(--lightblue);
     background: var(--lightgrey);
     border-radius: 0.5rem;
-    width: 30rem;
-    height: 6rem;
-  }
-
-  label {
-    display: flex;
-    align-items: center;
-    color: var(--darkblue);
-    font-weight: bold;
   }
 
   placeholder {
