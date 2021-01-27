5f1d5d2b423ba2531d0b56501c47904cbb63087d
Sophie Stammers <sophie.stammers@gmail.com>
2021-01-27 20:10:39 +0000

    test: added for secondary button onClick

--- src/components/SecondaryButton/SecondaryButton.test.jsx
+++ src/components/SecondaryButton/SecondaryButton.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 import SecondaryButton from './SecondaryButton';
 
@@ -8,5 +9,15 @@ describe('SecondaryButton', () => {
     render(<SecondaryButton content="Hello" />);
 
     expect(screen.getByText('Hello'));
+  });
+
+  test('calls onClick prop when clicked', () => {
+    const handleClick = jest.fn();
+
+    render(<SecondaryButton content="Hello" onClick={handleClick} />);
+
+    fireEvent.click(screen.getByRole('button'));
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
