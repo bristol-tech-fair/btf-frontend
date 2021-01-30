75a80911f806ff7f708f458e1f40aae49d4fbf88
r1903 <55586316+r1903@users.noreply.github.com>
2021-01-30 18:59:21 +0000

    refactor: added default color prop value

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { LogoWrapper } from './Logo.styles';
 
-const Logo = ({ color }) => {
+const Logo = ({ color = 'red' }) => {
   return <LogoWrapper color={color} />;
 };
 
