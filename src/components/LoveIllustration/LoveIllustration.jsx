7e9742d47be6de21470bfaf14440d0cb940587ea
mmkobylin <mmkobylin@gmail.com>
2021-02-01 17:42:10 +0000

    love illustration SVG successfully added to jsx file; styles changed to minimum

--- src/components/LoveIllustration/LoveIllustration.jsx
+++ src/components/LoveIllustration/LoveIllustration.jsx
@@ -5,7 +5,21 @@ import { LoveIllustrationWrapper } from './LoveIllustration.styles';
 const LoveIllustration = ({ value }) => {
   return (
     <LoveIllustrationWrapper>
-      <p>{value}</p>
+      <title id="loveTitle">Love Illustration</title>
+      <desc id="loveDesc"> Blue and pink love sign. </desc>
+      <svg
+        width="340"
+        height="162"
+        viewBox="0 0 340 162"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M310.512 0H29.488C13.2022 0 0 13.1771 0 29.432V132.568C0 148.823 13.2022 162 29.488 162H310.512C326.798 162 340 148.823 340 132.568V29.432C340 13.1771 326.798 0 310.512 0Z"
+          fill="#F2F2F2"
+        />
+      </svg>
+      <p> {value} </p>
     </LoveIllustrationWrapper>
   );
 };
--- src/components/LoveIllustration/LoveIllustration.styles.js
+++ src/components/LoveIllustration/LoveIllustration.styles.js
@@ -2,8 +2,4 @@ import styled from 'styled-components';
 
 export const LoveIllustrationWrapper = styled.div`
   padding: var(--small-space);
-  border: 1rem solid var(--lightblue);
-  border-left: 1.2rem solid var(--lightblue);
-  border-right: 1.2rem solid var(--lightblue);
-  border-radius: 1.5rem;
 `;
