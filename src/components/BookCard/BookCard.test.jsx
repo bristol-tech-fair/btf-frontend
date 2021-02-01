4c43543202f439f0a5743e639aab248e9e02a56b
Shane Mitchell <shaneSC@gmail.com>
2021-02-01 19:10:48 +0000

    test: add BookCard component tests

--- src/components/BookCard/BookCard.test.jsx
+++ src/components/BookCard/BookCard.test.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import coverImg from './book-of-making-cover.jpg';
 
 import BookCard from './BookCard';
 
 describe('BookCard', () => {
   test('renders BookCard component', () => {
-    render(<BookCard value="Hello" />);
+    render(
+      <BookCard title="Book of Making" author="J. Doe" cover={coverImg} />
+    );
 
-    expect(screen.getByText('Hello'));
+    expect(screen.getByText('Book of Making'));
+    expect(screen.getByText('by J. Doe'));
+  });
+
+  test('image renders correctly', () => {
+    render(
+      <BookCard title="Book of Making" author="J. Doe" cover={coverImg} />
+    );
+
+    const image = screen.getByAltText('Book of Making');
+    expect(image.src).toContain('.jpg');
   });
 });
