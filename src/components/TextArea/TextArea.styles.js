66b25a6be1fad7519539215b422bd512e511aa0f
Cath Howard <cath.m.howard@gmail.com>
2021-02-01 13:28:59 +0000

    styles: replicated styles from TextInput component

--- src/components/TextArea/TextArea.styles.js
+++ src/components/TextArea/TextArea.styles.js
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
 
 export const TextAreaWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
   textarea {
-    display: block;
+    padding: var(--small-space);
+    border: 0.1rem solid var(--lightblue);
     background: var(--lightgrey);
-    border: 1px solid var(--lightblue);
-    box-sizing: border-box;
     border-radius: 0.5rem;
+  }
+
+  placeholder {
+    color: var(--darkgrey);
+    font-size: 1rem;
   }
 `;
--- src/components/TextButton/TextButton.styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const TextButtonWrapper = styled.div`
-  padding: var(--small-space);
-  fontcolor: var(--lightblue);
-`;
