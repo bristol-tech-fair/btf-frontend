eb31cb4129410443798fbc84f7cede1012e8f80d
EdFildes <43066140+EdFildes@users.noreply.github.com>
2021-02-03 16:56:45 +0000

    Added LyingRobot component to Illustrations directory

---
+++ src/components/Illustration/LyingRobot/LyingRobot.stories.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import LyingRobot from './LyingRobot';
+
+export default {
+  title: 'Components/LyingRobot',
+  component: LyingRobot
+};
+export const LyingRobotTest = () => <LyingRobot />;
