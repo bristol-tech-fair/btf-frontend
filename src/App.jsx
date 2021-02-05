25f32dc3acd346436438c99898966324ba3081ee
r1903 <55586316+r1903@users.noreply.github.com>
2021-02-05 22:37:06 +0000

    changed App component to test routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Theme from './theme';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Router from './router';
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Theme />
-      <h1>h1</h1>
+      {/*  <h1>h1</h1>
       <h2>h2</h2>
       <h3>h3</h3>
       <h4>h4</h4>
       <p>p</p>
-      <a href="#/">a</a>
-    </>
+      <a href="#/">a</a> */}
+      <Header />
+      <Router />
+      <Footer />
+    </BrowserRouter>
   );
 };
 
