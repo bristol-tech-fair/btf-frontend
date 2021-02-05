e5b783c4ebd1c8894eab0a81b4ab8eb1d96de71c
r1903 <55586316+r1903@users.noreply.github.com>
2021-02-05 22:35:44 +0000

    feat:added router to application

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Terms from '../pages/Terms';
+import Policy from '../pages/Policy';
+import Books from '../pages/Books';
+import Clubs from '../pages/Clubs';
+import Competetions from '../pages/Competetions';
+import Events from '../pages/Events';
+import Resources from '../pages/Resources';
+import ResourcePage from '../pages/ResourcePage';
+import Posts from '../pages/Posts';
+import PostPage from '../pages/PostPage';
+import CodeOfConduct from '../pages/CodeOfConduct';
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/policy" exact component={Policy} />
+      <Route path="/terms" exact component={Terms} />
+      <Route path="/books" component={Books} />
+      <Route path="/clubs" component={Clubs} />
+      <Route path="/competetions" component={Competetions} />
+      <Route path="/codeofconduct" component={CodeOfConduct} />
+      <Route path="/events" component={Events} />
+      <Route exact path="/resources" component={Resources} />
+      <Route path="/resources/:title" component={ResourcePage} />
+      <Route exact path="/posts" component={Posts} />
+      <Route path="/posts/:title" component={PostPage} />
+    </Switch>
+  );
+}
+
+export default Router;
