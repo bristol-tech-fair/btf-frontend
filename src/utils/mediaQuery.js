2ab54b817bbcb7b563b8a6a46ebab0a6c30dab75
r1903 <55586316+r1903@users.noreply.github.com>
2021-02-10 20:08:17 +0000

    fix:fixing typo mistake

--- src/utils/mediaQuery.js
+++ src/utils/mediaQuery.js
@@ -1,6 +1,6 @@
 const mediaQuery = {
   tablet: `mediaQuery (min-width: 600px)`,
-  destop: `mediaQuery (min-width: 1024px)`
+  desktop: `mediaQuery (min-width: 1024px)`
 };
 
 export default mediaQuery;
