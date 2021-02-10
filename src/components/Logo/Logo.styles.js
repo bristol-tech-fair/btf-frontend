ff80d5ae219592f7bda88938b93740ab0b05b328
r1903 <55586316+r1903@users.noreply.github.com>
2021-02-10 22:23:44 +0000

    added mediaquery helper function  and used same in Logo component to make it responsive

--- src/components/Logo/Logo.styles.js
+++ src/components/Logo/Logo.styles.js
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 import LogoRed from '../../images/logoRed.png';
 import LogoBlue from '../../images/logoBlue.png';
+import mediaQuery from '../../utils/mediaQuery';
 
 export const LogoWrapper = styled.div`
   background: url(${(props) => (props.color === 'red' ? LogoRed : LogoBlue)}) 0
     0 / contain no-repeat;
-  width: 18.8rem;
-  height: 8.2rem;
+  width: 6.7rem;
+  height: 3rem;
+  ${mediaQuery.tablet} {
+    width: 9.128rem;
+    height: 4rem;
+  }
   &:hover {
     background: url(${(props) => (props.color === 'red' ? LogoBlue : LogoRed)})
       0 0 / contain no-repeat;
