5da7961e1a1cd4687ac6eeb789294348bbe15ffe
Shane Mitchell <shaneSC@gmail.com>
2021-02-11 18:18:49 +0000

    chore: fix typo

--- src/components/BookCard/BookCard.test.jsx
+++ src/components/BookCard/BookCard.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-add;
+
 import BookCard from './BookCard';
 
 describe('BookCard', () => {
