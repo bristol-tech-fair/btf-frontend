3f15402ab5a64842dbe8457fabab18419ced6561
Shane Mitchell <shaneSC@gmail.com>
2021-02-13 13:31:49 +0000

    feat: add modal-shadow CSS variable

--- src/theme/shadows.styles.js
+++ src/theme/shadows.styles.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const Shadows = createGlobalStyle`
   :root {
     --card-shadow: 0 1rem 1rem rgba(0, 0, 0, 0.1);
+    --modal-shadow: 0 0 6rem rgba(0, 0, 0, 0.5);
   }
 `;
 
