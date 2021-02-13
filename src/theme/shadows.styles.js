9ab8b331b06edbb7c99cab2afbd33c2176b5d6df
Shane Mitchell <shaneSC@gmail.com>
2021-02-13 13:02:47 +0000

    feat: add shadow theme files

---
+++ src/theme/shadows.styles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Shadows = createGlobalStyle`
+  :root {
+    --card-shadow: 0 1rem 1rem rgba(0, 0, 0, 0.1);
+  }
+`;
+
+export default Shadows;
