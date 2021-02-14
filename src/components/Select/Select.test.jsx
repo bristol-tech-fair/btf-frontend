d7fa97ecdf6712eb1d353a5ade5f9cd5f130e1db
Victoria Herod <vicky_herod@live.com>
2021-02-14 17:39:51 +0000

    tests: Began setting up tests to ensure onBlur rendering and functioning as expected

--- src/components/Select/Select.test.jsx
+++ src/components/Select/Select.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 import Select from './Select';
 
@@ -8,5 +9,27 @@ describe('Select', () => {
     render(<Select value="Hello" />);
 
     expect(screen.getByText('Hello'));
+  });
+
+  test('onBlur prop called when clicking away from component', () => {
+    const handleChange = jest.fn();
+
+    render(
+      <Select
+        name="test name"
+        label="test label"
+        defaultValue="test default value"
+        options={[
+          { optionValue: 'a', optionLabel: 'b' },
+          { optionValue: 'c', optionLabel: 'd' },
+          { optionValue: 'e', optionLabel: 'f' },
+        ]}
+        onBlur={handleChange}
+      />
+    );
+
+    fireEvent.blur(screen.getByRole('select'));
+
+    expect(handleChange).toHaveBeenCalledTimes(1);
   });
 });
