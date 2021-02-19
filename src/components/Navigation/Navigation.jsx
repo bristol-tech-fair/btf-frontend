259dfe4210af92278a77861acdef85d2574660ed
Sophie Stammers <sophie.stammers@gmail.com>
2021-02-19 14:30:13 +0000

    feat: Logo and text button added to nav with flex container

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { NavigationWrapper } from './Navigation.styles';
 
+import Logo from '../Logo';
+import TextButton from '../TextButton';
+
 const Navigation = ({ value }) => {
   return (
     <NavigationWrapper>
+      <Logo color="red" />
       <p>{value}</p>
+      <TextButton content="Add a resource" />
     </NavigationWrapper>
   );
 };
--- src/components/Navigation/Navigation.styles.js
+++ src/components/Navigation/Navigation.styles.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const NavigationWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding: var(--small-space);
   border: 0.1rem solid var(--logopink);
 `;
