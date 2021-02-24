190700607e6241300b52e3f46cb8e1991b03c071
Cath Howard <cath.m.howard@gmail.com>
2021-02-24 19:57:07 +0000

    fix: error removed importt statement

--- src/theme/index.js
+++ src/theme/index.js
@@ -3,7 +3,6 @@ import Init from './init.styles';
 import Palette from './palette.styles';
 import Spacings from './spacings.styles';
 import Typography from './typography.styles';
-import InputWrapper from './inputs.styles';
 
 const Theme = () => {
   return (
