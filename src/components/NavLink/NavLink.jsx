e59fac5d402440c49adc93bb8916ee7f09b94c63
Sophie Stammers <sophie.stammers@gmail.com>
2021-03-10 15:16:28 +0000

    feat: 'to' attribute added as prop

--- src/components/NavLink/NavLink.jsx
+++ src/components/NavLink/NavLink.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLinkWrapper } from './NavLink.styles';
 
-const NavLink = ({ value }) => {
-  return (
-    <NavLinkWrapper>
-      <p>{value}</p>
-    </NavLinkWrapper>
-  );
+const NavLink = ({ value, path }) => {
+  return <NavLinkWrapper to={path}>{value}</NavLinkWrapper>;
 };
 
 NavLink.propTypes = {
-  value: PropTypes.string.isRequired
+  value: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired
 };
 
 export default NavLink;
