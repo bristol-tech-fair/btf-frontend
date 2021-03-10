a93fffd3a8e75ecd0c3beb76a71da452c8d5819d
Tomasz <admtomas@gmail.com>
2021-03-10 12:50:31 +0000

    Testing code

---
+++ src/components/Banners/Medium/Medium.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import Medium from './Medium';
+
+describe('Medium', () => {
+  test('renders Medium component', () => {
+    render(<Medium />);
+
+    screen.debug();
+  });
+});
---
+++ src/components/Banners/Small/Small.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Small from './Small';
+
+export default {
+  title: 'Components/Banners/Small',
+  component: Small
+};
+
+export const TestSmall = () => <Small color="midblue" />;
