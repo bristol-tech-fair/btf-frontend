dbb284ff549817bfcf7812bec0bc11f8706ee20c
Sophie Stammers <sophie.stammers@gmail.com>
2021-03-10 15:25:10 +0000

    style: NavLink styling added

--- src/components/NavLink/NavLink.styles.js
+++ src/components/NavLink/NavLink.styles.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
 
 export const NavLinkWrapper = styled.div`
+  font-size: 1.6rem;
+  line-height: 2.08rem;
+  color: var(--darkburgundy);
   padding: var(--small-space);
-  border: 0.1rem solid var(--logopink);
+
+  &:hover {
+    color: var(--darkpink);
+  }
+
+  &:active,
+  &:focus {
+    color: var(--darkpink);
+  }
 `;
