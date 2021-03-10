06907d0b6a857a34ca1eb544bbc8449aa9078697
Sophie Stammers <sophie.stammers@gmail.com>
2021-03-10 15:45:01 +0000

    refactor: NavLink text center aligned

--- src/components/NavLink/NavLink.styles.js
+++ src/components/NavLink/NavLink.styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const NavLinkWrapper = styled.div`
   font-size: 1.6rem;
   line-height: 2.08rem;
+  text-align: center;
   color: var(--darkburgundy);
   padding: var(--small-space);
 
