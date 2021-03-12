9db76db486823c76ade038a6793a9e3ebe037fe7
Tomasz Amanowicz <47845850+admtomas@users.noreply.github.com>
2021-03-12 02:58:39 +0000

    Update Banners.styles.js

--- src/components/Banners/Banners.styles.js
+++ src/components/Banners/Banners.styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const SVG = styled.svg.attrs(() => ({
-  height: 'inherit',
-  width: 'inherit',
+  height: 'auto',
+  width: '100%',
   version: '1.1',
   xmlns: 'http://www.w3.org/2000/svg',
   xmlnsXlink: 'http://www.w3.org/1999/xlink'
