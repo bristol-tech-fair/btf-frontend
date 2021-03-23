670dded6615c158b3e02e91cb775a4ab089a3233
Tomasz <admtomas@gmail.com>
2021-03-23 11:26:06 +0000

    Unecesaryb code removed

--- src/components/Banners/Banners.styles.js
+++ src/components/Banners/Banners.styles.js
@@ -8,9 +8,4 @@ export const SVG = styled.svg.attrs(() => ({
   xmlnsXlink: 'http://www.w3.org/1999/xlink'
 }))`
   fill: ${({ color }) => `var(--${color})`};
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
 `;
