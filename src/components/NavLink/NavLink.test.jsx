26ec678639d7a682b2cd5c9efb096ebe87585fe6
r1903 <55586316+r1903@users.noreply.github.com>
2021-03-27 22:57:05 +0000

    test:added test for NavLink component

--- src/components/NavLink/NavLink.test.jsx
+++ src/components/NavLink/NavLink.test.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 
 import NavLink from './NavLink';
 
 describe('NavLink', () => {
   test('renders NavLink component', () => {
-    render(<NavLink content="Hello" />);
+    render(
+      <BrowserRouter>
+        <NavLink content="Test" path="/" />
+      </BrowserRouter>
+    );
 
-    expect(screen.getByText('Hello'));
+    expect(screen.getByText('Test'));
+  });
+
+  test('renders NavLink link properly', () => {
+    render(
+      <BrowserRouter>
+        <NavLink value="Test" path="/" />
+      </BrowserRouter>
+    );
+
+    const link = screen.getByRole('link');
+    expect(link.getAttribute('href')).toBe('/');
   });
 });
