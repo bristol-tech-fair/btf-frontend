3354e1334ae45c8ce10c89f589adb146cdf6da5c
r1903 <55586316+r1903@users.noreply.github.com>
2021-03-27 23:00:39 +0000

    style:added new styles to handle mobile and desltop view

--- src/components/Navigation/Navigation.styles.js
+++ src/components/Navigation/Navigation.styles.js
@@ -1,9 +1,51 @@
 import styled from 'styled-components';
+import mediaQuery from '../../utils/mediaQuery';
 
 export const NavigationWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: var(--small-space);
-  border: 0.1rem solid var(--logopink);
+  padding: var(--smallest-space), var(--small-space);
+  background: var(--white);
+
+  ${mediaQuery.tablet} {
+    padding: 20px;
+  }
+`;
+
+export const NavMenu = styled.div`
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  background: var(--white);
+  display: ${(props) => (props.click === true ? 'flex' : 'none')};
+
+  ${mediaQuery.desktop} {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    position: static;
+    width: auto;
+  }
+`;
+
+export const EndLink = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  margin: 0 var(--smallest-space);
+}
+`;
+
+export const MenuIcon = styled.div`
+  z-index: 99;
+  margin-left: var(--small-space);
+
+  ${mediaQuery.desktop} {
+    display: none;
+  }
 `;
