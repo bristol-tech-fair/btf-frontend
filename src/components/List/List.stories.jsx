cec0e61a232d817e25486a80675e53073962736f
Cath Howard <cath.m.howard@gmail.com>
2021-04-20 21:42:51 +0100

    Updated List.stories

--- src/components/List/List.stories.jsx
+++ src/components/List/List.stories.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import List from './List';
+import ClubCard from '../ClubCard';
 
 export default {
   title: 'Components/List',
   component: List
 };
 
-export const HelloWorld = () => <List value="Hello world!" />;
+export const ClubCardList = () => (
+  <List title="Clubs">
+    <Router>
+      <ClubCard
+        title="Codingbug"
+        to="/"
+        imgUrl="https://via.placeholder.com/240x140"
+      />
+    </Router>
+  </List>
+);
