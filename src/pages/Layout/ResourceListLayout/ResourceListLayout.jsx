f000c49bfdad2cce59d1c61c008e318f5ca645c6
r1903 <55586316+r1903@users.noreply.github.com>
2021-05-08 19:08:04 +0100

    refactor:removed headercontainer

--- src/pages/Layout/ResourceListLayout/ResourceListLayout.jsx
+++ src/pages/Layout/ResourceListLayout/ResourceListLayout.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  LayoutContainer,
-  HeaderContainer,
-  MainContainer
-} from './ResourceListLayout.styles';
+import { LayoutContainer, MainContainer } from './ResourceListLayout.styles';
 import Navigation from '../../../components/Navigation';
 
 const ResourceListLayout = ({ children }) => {
   return (
     <LayoutContainer>
-      <HeaderContainer>
-        <Navigation />
-      </HeaderContainer>
+      <Navigation />
       <MainContainer>{children}</MainContainer>
     </LayoutContainer>
   );
--- src/pages/Layout/ResourceListLayout/ResourceListLayout.styles.js
+++ src/pages/Layout/ResourceListLayout/ResourceListLayout.styles.js
@@ -3,25 +3,15 @@ import mediaQuery from '../../../utils/mediaQuery';
 
 export const LayoutContainer = styled.div`
   background: var(--white);
-
   display: flex;
   flex-direction: column;
-  align-items: center;
   min-height: 100vh;
-
+  max-width: 144rem;
+  margin: auto;
   overflow: hidden;
 `;
 
-export const HeaderContainer = styled.div`
-  width: 100%;
-
-  ${mediaQuery.desktop} {
-    max-width: 144rem;
-  }
-`;
-
 export const MainContainer = styled.main`
-  max-width: 144rem;
   margin: var(--medium-space) 0;
   padding: 0 5px;
 
