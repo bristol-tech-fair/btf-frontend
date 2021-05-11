331939d514bac2c87ccc4ac6705389ea4b4f5bd2
r1903 <55586316+r1903@users.noreply.github.com>
2021-05-11 14:10:28 +0100

    feat:added basic styles and tag to component

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,17 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { PostCardWrapper } from './PostCard.styles';
+import {
+  PostCardWrapper,
+  Image,
+  DateCategoryWrapper,
+  Date,
+  Title,
+  IconWrapper,
+  MoreLink,
+  MoreLinkWrapper
+} from './PostCard.styles';
+import { ArrowCircleRight, Share, Bookmark } from '../Icons';
 
-const PostCard = ({ value }) => {
+const PostCard = ({
+  coverUrl,
+  category,
+  date,
+  title,
+  postLink,
+  shareLink,
+  bookmark
+}) => {
   return (
     <PostCardWrapper>
-      <p>{value}</p>
+      <Image src={coverUrl} alt={title} />
+      <DateCategoryWrapper>
+        <p>{category}</p>
+        <Date>{date}</Date>
+      </DateCategoryWrapper>
+      <Title>{title}</Title>
+      <IconWrapper>
+        <MoreLinkWrapper>
+          <ArrowCircleRight />
+          <MoreLink to={postLink}>Read more</MoreLink>
+        </MoreLinkWrapper>
+        <MoreLinkWrapper>
+          <a href={shareLink}>
+            <Share />
+          </a>
+          <a href={bookmark}>
+            <Bookmark />
+          </a>
+        </MoreLinkWrapper>
+      </IconWrapper>
     </PostCardWrapper>
   );
 };
 
 PostCard.propTypes = {
-  value: PropTypes.string.isRequired
+  coverUrl: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  postLink: PropTypes.string.isRequired,
+  shareLink: PropTypes.string.isRequired,
+  bookmark: PropTypes.string.isRequired
 };
 
 export default PostCard;
--- src/components/PostCard/PostCard.styles.js
+++ src/components/PostCard/PostCard.styles.js
@@ -1,6 +1,75 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Image = styled.img`
+  object-fit: cover;
+  width: 100%;
+  opacity: 0.5;
+`;
+
+export const Title = styled.h3`
+  line-height: 130%;
+  font-weight: 700;
+  padding: var(--smallest-space);
+  padding-left: var(--small-space);
+  color: var(--darkburgundy);
+`;
 
 export const PostCardWrapper = styled.div`
-  padding: var(--small-space);
-  border: 0.1rem solid var(--logopink);
+  display: flex;
+  flex-direction: column;
+  border: 0.1rem solid var(--lightblue);
+  border-radius: var(--smallest-space);
+  width: 258px;
+  background: var(--lightgrey);
+  &:hover ${Image} {
+    opacity: 1;
+  }
+  &:hover ${Title} {
+    color: #f28383;
+  }
+`;
+
+export const DateCategoryWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: var(--smallest-space) var(--smallest-space) 0 var(--smallest-space);
+  p {
+    font-weight: bold;
+  }
+`;
+
+export const Date = styled.p`
+  color: var(--lightblue);
+`;
+
+export const IconWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 var(--smallest-space) var(--smallest-space) var(--smallest-space);
+  svg {
+    width: 24px;
+    height: 24px;
+    path {
+      stroke: #737373;
+    }
+  }
+`;
+
+export const MoreLink = styled(Link)`
+  font-size: 12px;
+  color: var(--darkburgundy);
+  &:hover {
+    color: var(--darkburgundy);
+  }
+`;
+
+export const MoreLinkWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  a {
+    padding-left: 5px;
+  }
 `;
