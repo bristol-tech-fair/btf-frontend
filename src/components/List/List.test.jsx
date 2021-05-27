57a52dfbd66c46587573a43814f8008e48ccf31a
Tomasz <admtomas@gmail.com>
2021-05-27 17:23:25 +0100

    Test refactored for List component

--- src/components/List/List.test.jsx
+++ src/components/List/List.test.jsx
@@ -3,10 +3,24 @@ import { render, screen } from '@testing-library/react';
 
 import List from './List';
 
-describe('List', () => {
-  test('renders List component', () => {
-    render(<List title="Clubs" Component="ClubCard" />);
+const data = [
+  {
+    coverUrl: 'https://via.placeholder.com/240x140',
+    title: 'Codingbug',
+    to: '/'
+  },
+  {
+    coverUrl: 'https://via.placeholder.com/240x140',
+    title: 'Code Club',
+    to: '/'
+  }
+];
 
-    expect(screen.getByText('Clubs'));
+describe('List', () => {
+  test('renders List component with items passed correctly', () => {
+    const { getByRole } = render(<List data={data} Component="ClubCard" />);
+    const ul = getByRole('list');
+    screen.debug();
+    expect(ul.children.length).toEqual(2);
   });
 });
