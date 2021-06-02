c1095ffc9139aa49673f1440d0d504e7b7a56aa8
Tomasz <admtomas@gmail.com>
2021-06-02 20:02:14 +0100

    Initial markup

--- src/pages/ResourceCardOpen/ResourceCardOpen.jsx
+++ src/pages/ResourceCardOpen/ResourceCardOpen.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import { ContentWrapper } from './ResourceCardOpen.styles';
+import { ContentWrapper, Info, ClickCounter } from './ResourceCardOpen.styles';
 import DefaultPageLayout from '../Layout/DefaultPageLayout';
 import { Large } from '../../components/Banners';
+import { Heart, HeartFull } from '../../components/Icons';
 
 const ResourceCardOpen = () => {
+  const condition = 2;
   return (
     <DefaultPageLayout>
-      <Large />
+      <Large color="logopink" />
       <ContentWrapper>
-        <p>electronic</p>
-        <h1>Title</h1>
-        <div>some rank</div>
-        <p />
+        <h3>Electronics</h3>
+        <h1>The Institution of Engineering</h1>
+
+        <Info>
+          <div>
+            <h4>Ages:</h4>
+            <div>
+              {condition > 0 ? <HeartFull /> : <Heart />}
+              {condition > 1 ? <HeartFull /> : <Heart />}
+              {condition > 2 ? <HeartFull /> : <Heart />}
+              {condition > 3 ? <HeartFull /> : <Heart />}
+              {condition > 4 ? <HeartFull /> : <Heart />}
+              <ClickCounter>| clicks</ClickCounter>
+            </div>
+          </div>
+
+          <p>
+            We support STEM teachers by providing a range of resources linked to
+            the UK curriculum and partnering with organisations to create
+            experiences that inspire teachers and students alike.
+          </p>
+        </Info>
+
         <div>button</div>
         <div />
         <div />
@@ -26,10 +47,6 @@ const ResourceCardOpen = () => {
 
 ResourceCardOpen.propTypes = {
   // bla: PropTypes.string,
-};
-
-ResourceCardOpen.defaultProps = {
-  // bla: 'test',
 };
 
 export default ResourceCardOpen;
--- src/pages/ResourceCardOpen/ResourceCardOpen.styles.js
+++ src/pages/ResourceCardOpen/ResourceCardOpen.styles.js
@@ -3,8 +3,27 @@ import styled from 'styled-components';
 export const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transform: translateY(calc(-1 * var(--medium-space)));
+  border: 2px solid green;
+
+  h3 {
+    transform: translateY(calc(-1 * var(--medium-space)));
+  }
 `;
 
-export const Wrapper = styled.div`
-  display: flex;
+export const ClickCounter = styled.p`
+  margin-left: 3px;
+`;
+
+export const Info = styled.div`
+  padding: var(--medium-space);
+  //text-align: center;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    padding: var(--smallest-space) 0;
+  }
 `;
