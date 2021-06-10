7e28a3ffe16546664008687cbc373f95c704ea5c
admtomas <admtomas@gmail.com>
2021-06-10 02:28:13 +0100

    Added descriotion and refactored closing form function based is it fron or back card

--- src/components/SubmitResourceForm/SubmitResourceForm.jsx
+++ src/components/SubmitResourceForm/SubmitResourceForm.jsx
@@ -34,167 +34,172 @@ import TextButton from '../TextButton';
 import { Robot } from '../Illustration';
 import { Cross, File, Youtube, Image, ArrowLeft } from '../Icons';
 
-const SubmitResourceForm = forwardRef(
-  ({ selectAges, selectCategory }, ref, props) => {
-    const [isFlipped, setIsFlipped] = useState(false);
-    const [ages, setAges] = useState([]);
-    const [cat, setCat] = useState([]);
-    //* ----Toggler functionality
-    const [close, setClose] = useState(false);
-    const openPopup = () => {
-      setClose(!close);
+const SubmitResourceForm = forwardRef(({ selectAges, selectCategory }, ref) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [ages, setAges] = useState([]);
+  const [cat, setCat] = useState([]);
+  const [close, setClose] = useState(false);
+  //* ----Toggler functionality
+
+  const closeCard = () => {
+    setClose(!close);
+  };
+
+  const closeBackCard = () => {
+    setClose(!close);
+    setIsFlipped(!isFlipped);
+  };
+
+  const openPopup = () => {
+    setClose(!close);
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      openPopup
     };
+  });
 
-    useImperativeHandle(ref, () => {
-      return {
-        openPopup
-      };
-    });
+  //* Setting up options for select tags
 
-    useEffect(() => {
-      setAges(selectAges);
-    }, [selectAges]);
+  useEffect(() => {
+    setAges(selectAges);
+  }, [selectAges]);
 
-    useEffect(() => {
-      setCat(selectCategory);
-    }, [selectCategory]);
+  useEffect(() => {
+    setCat(selectCategory);
+  }, [selectCategory]);
 
-    const {
-      register,
-      handleSubmit,
-      formState: { errors }
-    } = useForm();
+  //* React-hook-form  functionality
 
-    const onSubmit = (formData) => {
-      setTimeout(() => {
-        setIsFlipped(!isFlipped);
-        console.log(formData);
-      }, 1000);
-    };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
 
-    return (
-      <PopupContainer close={close} {...props}>
-        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-          <FrontCard>
-            <MobileNav>
-              <Navigation />
-            </MobileNav>
-            <CloseButton onClick={() => setClose(!close)}>
-              <Cross />
-            </CloseButton>
-            <Header>Submit your resource</Header>
-            <Form onSubmit={handleSubmit(onSubmit)}>
-              <TextInput
-                id="title"
-                name="title"
-                label="Title*"
-                placeholder="Resource name"
-                {...register('title', { required: true })}
-              />
-              {errors.title && <span id="err-msg">This Field is required</span>}
-              <Select
-                id="Category"
-                name="Category"
-                label="Category*"
-                placeholder="Choose category..."
-                options={cat}
-                {...register('category', { required: true })}
-              />
-              {errors.category && (
-                <span id="err-msg">This Field is required</span>
-              )}
-              <Select
-                id="ages"
-                name="ages"
-                label="Ages*"
-                placeholder="Choose age group..."
-                options={ages}
-                {...register('ages', { required: true })}
-              />
-              {errors.ages && <span id="err-msg">This Field is required</span>}
-              <TextArea
-                id="descriptionInput"
-                name="textInput"
-                label="Description"
-                placeholder="Write what this resource is about"
-                {...register('description')}
-              />
-              <Attachments>Attachments</Attachments>
-              <UploadsContainer>
-                <FileToUpload>
-                  <File />
-                  <TextInput
-                    type="file"
-                    label="File"
-                    id="document"
-                    name="document"
-                    {...register('document')}
-                  />
-                </FileToUpload>
-                <FileToUpload>
-                  <Youtube />
-                  <TextInput
-                    type="file"
-                    label="Video"
-                    id="video"
-                    name="video"
-                    {...register('video')}
-                  />
-                </FileToUpload>
-                <FileToUpload>
-                  <Image />
-                  <TextInput
-                    type="file"
-                    label="Image"
-                    id="image"
-                    name="image"
-                    {...register('image')}
-                  />
-                </FileToUpload>
-              </UploadsContainer>
-              <ButtonContainer>
-                <TertiaryButton type="submit" content="Submit" />
-              </ButtonContainer>
-            </Form>
-            <ReturnButton>
-              <ArrowLeft />
-              <TextButton
-                type="button"
-                content="Back"
-                onClick={() => setClose(!close)}
-              />
-            </ReturnButton>
-            <Info>Fields marked with * are mandatory.</Info>
-          </FrontCard>
+  const onSubmit = (formData, e) => {
+    e.target.reset();
+    setTimeout(() => {
+      setIsFlipped(!isFlipped);
+      console.log(formData);
+    }, 1000);
+  };
 
-          <BackCard>
-            <MobileNav>
-              <Navigation />
-            </MobileNav>
-            <CloseButton type="button" onClick={() => setClose(!close)}>
-              <Cross />
-            </CloseButton>
-            <ImageContainer>
-              <Robot />
-            </ImageContainer>
-            <Header>Thank you for submitting your resource!</Header>
-            <ReturnButton>
-              <ArrowLeft />
-              <TextButton
-                type="button"
-                content="Back"
-                onClick={() => setClose(!close)}
-              />
-            </ReturnButton>
-            <FooterContainer>
-              <Footer />
-            </FooterContainer>
-          </BackCard>
-        </ReactCardFlip>
-      </PopupContainer>
-    );
-  }
-);
+  return (
+    <PopupContainer close={close}>
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+        <FrontCard>
+          <MobileNav>
+            <Navigation />
+          </MobileNav>
+          <CloseButton onClick={closeCard}>
+            <Cross />
+          </CloseButton>
+          <Header>Submit your resource</Header>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <TextInput
+              id="title"
+              name="title"
+              label="Title*"
+              placeholder="Resource name"
+              {...register('title', { required: true })}
+            />
+            {errors.title && <span id="err-msg">This Field is required</span>}
+            <Select
+              id="Category"
+              name="Category"
+              label="Category*"
+              placeholder="Choose category..."
+              options={cat}
+              {...register('category', { required: true })}
+            />
+            {errors.category && (
+              <span id="err-msg">This Field is required</span>
+            )}
+            <Select
+              id="ages"
+              name="ages"
+              label="Ages*"
+              placeholder="Choose age group..."
+              options={ages}
+              {...register('ages', { required: true })}
+            />
+            {errors.ages && <span id="err-msg">This Field is required</span>}
+            <TextArea
+              id="descriptionInput"
+              name="textInput"
+              label="Description"
+              placeholder="Write what this resource is about"
+              {...register('description')}
+            />
+            <Attachments>Attachments</Attachments>
+            <UploadsContainer>
+              <FileToUpload>
+                <File />
+                <TextInput
+                  type="file"
+                  label="File"
+                  id="document"
+                  name="document"
+                  {...register('document')}
+                />
+              </FileToUpload>
+              <FileToUpload>
+                <Youtube />
+                <TextInput
+                  type="file"
+                  label="Video"
+                  id="video"
+                  name="video"
+                  {...register('video')}
+                />
+              </FileToUpload>
+              <FileToUpload>
+                <Image />
+                <TextInput
+                  type="file"
+                  label="Image"
+                  id="image"
+                  name="image"
+                  {...register('image')}
+                />
+              </FileToUpload>
+            </UploadsContainer>
+            <ButtonContainer>
+              <TertiaryButton type="submit" content="Submit" />
+            </ButtonContainer>
+          </Form>
+          <ReturnButton>
+            <ArrowLeft />
+            <TextButton type="button" content="Back" onClick={closeCard} />
+          </ReturnButton>
+          <Info>Fields marked with * are mandatory.</Info>
+        </FrontCard>
+
+        <BackCard>
+          <MobileNav>
+            <Navigation />
+          </MobileNav>
+          <CloseButton type="button" onClick={closeBackCard}>
+            <Cross />
+          </CloseButton>
+          <ImageContainer>
+            <Robot />
+          </ImageContainer>
+          <Header>Thank you for submitting your resource!</Header>
+          <ReturnButton>
+            <ArrowLeft />
+            <TextButton type="button" content="Back" onClick={closeBackCard} />
+          </ReturnButton>
+          <FooterContainer>
+            <Footer />
+          </FooterContainer>
+        </BackCard>
+      </ReactCardFlip>
+    </PopupContainer>
+  );
+});
 
 SubmitResourceForm.propTypes = {
   selectAges: PropTypes.arrayOf(
