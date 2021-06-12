2be4315e6ef0b3c397c682f20834097742dc55a6
admtomas <admtomas@gmail.com>
2021-06-12 02:25:03 +0100

    code cleanup

--- src/pages/ResourceCardOpen/ResourceCardopen.stories.jsx
+++ src/pages/ResourceCardOpen/ResourceCardopen.stories.jsx
@@ -10,16 +10,6 @@ export default {
 export const ResourceCardOpenTest = () => (
   <BrowserRouter>
     <ResourceCardOpen
-      dropdown={[
-        { value: '8-11', label: '8-11' },
-        { value: '12-15', label: '12-15' },
-        { value: '16-18', label: '16-18' }
-      ]}
-      selectCategory={[
-        { value: 'maths', label: 'maths' },
-        { value: 'coding', label: 'coding' },
-        { value: 'engineering', label: 'engineering' }
-      ]}
       facebookLink="https://www.wp.pl/"
       resourceLink="https://www.wp.pl/"
       twitterLink="https://www.wp.pl/"
