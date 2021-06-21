4ba63c1541ce4ac5645ed0c9daf494f759809ca5
Cath Howard <cath.m.howard@gmail.com>
2021-06-21 21:55:50 +0100

    Add styles for filter component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FilterWrapper, SelectWrapper, ButtonContainer } from './Filter.styles';
+import { FilterWrapper, ButtonContainer } from './Filter.styles';
 import Select from '../Select';
 import PrimaryButton from '../PrimaryButton';
 import TextButton from '../TextButton';
@@ -7,21 +7,30 @@ import TextButton from '../TextButton';
 const Filter = () => {
   return (
     <FilterWrapper>
-      <SelectWrapper>
-        <Select
-          name="Age Group"
-          id="ages"
-          label="Filter"
-          placeholder="Age group"
-          options={[
-            { value: '6-18', label: '6-18 years' },
-            { value: '6-12', label: '6-12 years' },
-            { value: '12-18', label: '12-18 years' }
-          ]}
-        />
-        <PrimaryButton content="Find resources" />
-      </SelectWrapper>
+      <Select
+        name="Age Group"
+        id="ages"
+        label="Filter"
+        placeholder="Age group"
+        options={[
+          { value: '6-18', label: '6-18 years' },
+          { value: '6-12', label: '6-12 years' },
+          { value: '12-18', label: '12-18 years' }
+        ]}
+      />
+      <Select
+        name="Any topic"
+        id="topic"
+        label=""
+        placeholder="Any topic"
+        options={[
+          { value: 'coding', label: 'Coding' },
+          { value: 'maths', label: 'Maths' },
+          { value: 'electronics', label: 'Electronics' }
+        ]}
+      />
       <ButtonContainer>
+        <PrimaryButton content="Find resources" />
         <TextButton content="Clear Filters" />
       </ButtonContainer>
     </FilterWrapper>
--- src/components/Filter/Filter.styles.js
+++ src/components/Filter/Filter.styles.js
@@ -3,27 +3,28 @@ import mediaQuery from '../../utils/mediaQuery';
 
 export const FilterWrapper = styled.div`
   padding: var(--small-space);
-  text-align: center;
-`;
-
-export const SelectWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  text-align: center;
   ${mediaQuery.tablet} {
     height: 6rem;
     flex-direction: row;
     width: 80%;
+    text-align: left;
   }
   ${mediaQuery.desktop} {
     height: 7rem;
     flex-direction: row;
     width: 90%;
+    text-align: left;
   }
 `;
 
 export const ButtonContainer = styled.div`
   margin-top: var(--smallest-space);
+  display: flex;
+  flex-direction: column;
   ${mediaQuery.desktop} {
-    text-align: right;
+    text-align: left;
   }
 `;
