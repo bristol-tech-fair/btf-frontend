54d1de139e207ee0ff2a4e9fcd20e9068d42eaf8
Tomasz <admtomas@gmail.com>
2021-07-16 12:23:29 +0100

    paths created to match current names of web pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,62 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-// import Home from '../pages/Home';
-// import About from '../pages/About';
-// import Terms from '../pages/Terms';
-// import Policy from '../pages/Policy';
-// import Books from '../pages/Books';
-// import Clubs from '../pages/Clubs';
-// import Groups from '../pages/Groups';
-// import Competetions from '../pages/Competetions';
-// import Events from '../pages/Events';
-// import Resources from '../pages/Resources';
-// import ResourcePage from '../pages/ResourcePage';
-// import Posts from '../pages/Posts';
-// import PostPage from '../pages/PostPage';
-// import CodeOfConduct from '../pages/CodeOfConduct';
-// import AddResource from '../pages/AddResource';
-// import ModalContext from '../context/ModalContext';
+import About from '../pages/About';
+import BookList from '../pages/BookList';
+import Clubs from '../pages/Clubs';
+import CodeOfConduct from '../pages/CodeOfConduct';
+import Competitions from '../pages/Competitions';
+import Contact from '../pages/Contact';
+import CookiePolicy from '../pages/CookiePolicy';
+import Events from '../pages/Events';
+import HomePage from '../pages/HomePage';
+import Posts from '../pages/Posts';
+import PostsOpen from '../pages/PostsOpen';
+import SupportGroups from '../pages/SupportGroups';
+import TermsAndConditions from '../pages/TandC';
 
 function Router() {
   return (
     <>
       <Switch>
-        <Route path="/" exact>
-          {/* <Home /> */}
+        <Route exact path="/">
+          <HomePage />
         </Route>
-        <Route path="/about">{/* <About /> */}</Route>
-        <Route path="/cookiepolicy">{/* <Policy /> */}</Route>
-        <Route path="/termsandconditions">{/* <Terms /> */}</Route>
-        <Route path="/books">{/* <Books /> */}</Route>
-        <Route path="/clubs">{/* <Clubs /> */}</Route>
-        <Route path="/supportgroups">{/* <Groups /> */}</Route>
-        <Route path="/competitions">{/* <Competetions /> */}</Route>
-        <Route path="/codeofconduct">{/* <CodeOfConduct /> */}</Route>
-        <Route path="/events">{/* <Events /> */}</Route>
-        <Route exact path="/resources">
-          {/* <Resources /> */}
+        <Route path="/about">
+          <About />
         </Route>
-        <Route path="/resources/:title">{/* <ResourcePage /> */}</Route>
-        <Route exact path="/posts">
-          {/* <Posts /> */}
+        <Route path="/cookiepolicy">
+          <CookiePolicy />
         </Route>
-        <Route path="/posts/:title">{/* <PostPage /> */}</Route>
+        <Route path="/termsandconditions">
+          <TermsAndConditions />
+        </Route>
+        <Route path="/books">
+          <BookList />
+        </Route>
+        <Route path="/clubs">
+          <Clubs />
+        </Route>
+        <Route path="/supportgroups">
+          <SupportGroups />
+        </Route>
+        <Route path="/competitions">
+          <Competitions />
+        </Route>
+        <Route path="/codeofconduct">
+          <CodeOfConduct />
+        </Route>
+        <Route path="/events">
+          <Events />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/posts">
+          <Posts />
+        </Route>
+        <Route path="/posts/:title">
+          <PostsOpen />
+        </Route>
       </Switch>
     </>
   );
