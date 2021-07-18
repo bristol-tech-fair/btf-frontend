8f6731d755da94babfe22fcf5a051969a310c568
admtomas <admtomas@gmail.com>
2021-07-18 02:38:18 +0100

    updating branch with main branch

--- src/pages/ResourceCardOpen/ResourceCardopen.stories.jsx
+++ src/pages/ResourceCardOpen/ResourceCardopen.stories.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ResourceCardOpen from './ResourceCardOpen';
 
 export default {
-  title: 'Components/Pages/ResourceCardOpen',
+  title: 'Pages/ResourceCardOpen',
   component: ResourceCardOpen
 };
 
