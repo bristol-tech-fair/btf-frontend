d042ed5629886152d21a47714f2c0ff41602f53d
admtomas <admtomas@gmail.com>
2021-08-07 02:38:05 +0100

    storybook change

--- src/pages/LearningResources/LearningResources.stories.jsx
+++ src/pages/LearningResources/LearningResources.stories.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import LearningResources from './LearningResources';
 
 export default {
-  title: 'Components/Pages/LearningResources',
+  title: 'Pages/LearningResources',
   component: LearningResources
 };
 
