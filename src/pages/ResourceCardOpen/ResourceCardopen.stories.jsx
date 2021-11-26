830e22d2f735c939b3068ea50cbd056d70fefa08
Tomasz <admtomas@gmail.com>
2021-11-26 18:37:21 +0000

    Initial code for the page

--- src/pages/ResourceCardOpen/ResourceCardopen.stories.jsx
+++ src/pages/ResourceCardOpen/ResourceCardopen.stories.jsx
@@ -3,12 +3,19 @@ import { BrowserRouter } from 'react-router-dom';
 import ResourceCardOpen from './ResourceCardOpen';
 
 export default {
-  title: 'Components/Pages/ResourceCardOpen',
+  title: 'Pages/ResourceCardOpen',
   component: ResourceCardOpen
 };
 
 export const ResourceCardOpenTest = () => (
   <BrowserRouter>
-    <ResourceCardOpen />
+    <ResourceCardOpen
+      facebookLink="https://www.interia.pl/"
+      resourceLink="https://www.wp.pl/"
+      twitterLink="https://www.wp.pl/"
+      bookmarksLink="https://www.o2.pl/"
+      linkedInLink="https://www.o2.pl/"
+      contactSupport="mailto:admtomas@gmail.com"
+    />
   </BrowserRouter>
 );
