b6c451124d230967b272a772298ea6f21798fa05
Dave Jordan <le_dangereux@hotmail.co.uk>
2021-11-27 23:38:34 +0000

    Delete example component

--- src/components/Example/Example.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Example from './Example';
-
-export default {
-  title: 'Components/Example',
-  component: Example
-};
-
-export const HelloWorld = () => <Example value="Hello world!" />;
