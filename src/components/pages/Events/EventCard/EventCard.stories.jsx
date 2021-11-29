3e2881d5fb95103e299ee01d9d1de880ec067508
Dave Jordan <le_dangereux@hotmail.co.uk>
2021-11-29 15:12:05 +0000

    Fix multiline string carriage return that eslint complains about

--- src/components/pages/Events/EventCard/EventCard.stories.jsx
+++ src/components/pages/Events/EventCard/EventCard.stories.jsx
@@ -13,7 +13,6 @@ Default.args = {
   imgSrc: './assets/images/codingbug.png',
   date: '13/5/2021',
   title: 'Coding Bug Event',
-  desc:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
   url: 'www.bristoltechfair.com'
 };
