8a0fe0a90a36a86e97374e057e4913a20f486826
Tomasz Amanowicz <47845850+admtomas@users.noreply.github.com>
2021-12-11 02:45:14 +0000

    About page (#140)
    * Page content stylying
    
    * Final styling for responsive design
    
    * redundant code deleted

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -1,21 +1,95 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import DefaultPageLayout from 'components/common/Layout/DefaultPageLayout';
-import { AboutWrapper } from './About.styles';
+import SponsorCard from 'components/pages/About/SponsorCard';
+import List from 'components/common/Layout/List';
+import Spinner from 'components/common/Spinner';
+import { MainContainer, PageTitle, ListContainer } from './About.styles';
+import { LyingRobot, LoveIllustration } from '../../common/Illustrations';
 
-const About = ({ value }) => {
+const About = () => {
+  // TODO: Replace this temporary data with API call
+  const sponsorData = [
+    {
+      sponsorLogo: 'https://via.placeholder.com/240x140',
+      sponsorName: 'WTPageTitle'
+    },
+    {
+      sponsorLogo: 'https://via.placeholder.com/240x140',
+      sponsorName: 'WTH2'
+    },
+    {
+      sponsorLogo: 'https://via.placeholder.com/240x140',
+      sponsorName: 'WTH3'
+    },
+    {
+      sponsorLogo: 'https://via.placeholder.com/240x140',
+      sponsorName: 'WTH4'
+    },
+    {
+      sponsorLogo: 'https://via.placeholder.com/240x140',
+      sponsorName: 'WTH5'
+    }
+  ];
+
   return (
     <DefaultPageLayout>
-      <AboutWrapper>
-        <p>{value}</p>
-        <h1>This is the About page. It is here to check the Router</h1>
-      </AboutWrapper>
+      <MainContainer>
+        <PageTitle>About Us</PageTitle>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ad
+          impedit, nobis ducimus animi recusandae optio obcaecati ut amet
+          aliquam aliquid sunt quos enim eum consectetur incidunt voluptates
+          commodi architecto quod deleniti dolorum? Earum neque culpa fugit
+          perferendis magni minima pariatur ullam, aliquid eos facere, repellat
+          libero dolores! Odit, ratione?
+        </p>
+        <div>
+          <LyingRobot />
+        </div>
+
+        <PageTitle>Mission & Values </PageTitle>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. A neque
+          deserunt eligendi omnis odit labore in debitis repellendus minus
+          voluptates repellat, dolorum sunt maxime inventore cupiditate
+          distinctio harum mollitia incidunt delectus, facilis at. Officiis
+          culpa recusandae omnis! Aspernatur, odio blanditiis ex nihil hic
+          tempore aut facere, neque perferendis deleniti recusandae dolore ea
+          libero, nostrum iste pariatur vero. Repudiandae ea pariatur, velit
+          atque magnam eos enim placeat adipisci unde soluta maiores, corporis,
+          quasi ad laborum! A mollitia quis assumenda facere consequuntur
+          debitis, nam totam autem neque architecto cumque laborum est quo ipsa
+          laudantium temporibus amet. Voluptatem modi ad veniam laborum
+          obcaecati.
+        </p>
+        <div>
+          <LoveIllustration />
+        </div>
+
+        <PageTitle>Diversity & Inclusion</PageTitle>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto
+          illo blanditiis minima dolor cumque quia eos fugiat omnis aliquid
+          sequi et, nemo, dolorem modi tempore, dicta voluptatum porro. A
+          pariatur sed, eos eligendi quibusdam deserunt itaque ipsa quod eaque
+          facere neque suscipit, minus quae libero quidem ipsum doloremque enim
+          tenetur. Qui doloribus itaque dolorem libero consequuntur, sit nostrum
+          maxime? Inventore deserunt, dignissimos labore perspiciatis ullam
+          dolorum earum, eligendi libero dolores unde non voluptate itaque
+          exercitationem expedita velit iusto ea praesentium?
+        </p>
+
+        <PageTitle>Sponsors</PageTitle>
+        <ListContainer>
+          {sponsorData ? (
+            <List Component={SponsorCard} data={sponsorData} />
+          ) : (
+            <Spinner />
+          )}
+        </ListContainer>
+      </MainContainer>
     </DefaultPageLayout>
   );
-};
-
-About.propTypes = {
-  value: PropTypes.string.isRequired
 };
 
 export default About;
--- src/components/pages/About/About.styles.js
+++ src/components/pages/About/About.styles.js
@@ -1,6 +1,37 @@
 import styled from 'styled-components';
+import mediaQuery from 'utils/mediaQuery';
 
-export const AboutWrapper = styled.div`
-  padding: var(--small-space);
-  border: 0.1rem solid var(--logopink);
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  > p {
+    padding: var(--small-space);
+  }
+`;
+
+export const PageTitle = styled.h1`
+  margin: var(--small-space) 0;
+`;
+
+export const ListContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: var(--large-space);
+  width: 100%;
+
+  //* Style for List component
+  & > div {
+    padding: var(--smallest-space);
+    & > ul {
+      gap: var(--smallest-space);
+      ${mediaQuery.desktop} {
+        grid-template-columns: repeat(3, 1fr);
+        padding: var(--small-space);
+      }
+    }
+  }
 `;
